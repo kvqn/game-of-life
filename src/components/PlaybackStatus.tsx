3b9ce9794d62a1693d7eff88388fa7da9f224821
kvqn <guneetaggarwal@gmail.com>
2024-04-19 03:09:16 +0530

    style: minor styling

--- src/components/PlaybackStatus.tsx
+++ src/components/PlaybackStatus.tsx
@@ -9,18 +9,16 @@ export function PlaybackStatus() {
 
   return (
     <>
-      <div className="fixed bottom-2 right-2 flex items-center gap-2">
-        <p
-          className={cn("text-xl opacity-0 transition-opacity lg:opacity-0", {
-            "opacity-100": !playbackState,
-          })}
-        >
-          Paused
-        </p>
+      <div className="fixed bottom-2 right-2 flex cursor-pointer items-center gap-2 divide-x divide-gray-300 rounded-md border border-gray-300 bg-gray-100 text-xl shadow-md transition-colors hover:bg-gray-200">
+        {!playbackState && (
+          <div
+            className={"flex h-full w-auto items-center justify-center pl-2"}
+          >
+            Paused
+          </div>
+        )}
         <div
-          className={cn(
-            "flex h-10 w-10 cursor-pointer items-center justify-center rounded-md border border-gray-300 bg-gray-100 text-xl shadow-md transition-colors hover:bg-gray-200",
-          )}
+          className={"flex h-10 w-10 items-center justify-center"}
           onClick={() => {
             setPlaybackState(!playbackState)
             playbackRef.current = !playbackState
@@ -31,7 +29,7 @@ export function PlaybackStatus() {
       </div>
       <div
         className={cn(
-          "fixed bottom-2 left-1/2 -translate-x-1/2 text-xl opacity-0 transition-opacity",
+          "fixed bottom-2 left-1/2 -translate-x-1/2 select-none rounded-md border border-gray-300 bg-white px-4 py-1 text-xl opacity-0 shadow-md transition-opacity hover:opacity-20",
           {
             "lg:opacity-100": !playbackState,
           },
