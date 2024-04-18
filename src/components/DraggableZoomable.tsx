b4e5c92405c7ca3cfa613cbd8d736b8c05cf4410
kvqn <guneetaggarwal@gmail.com>
2024-04-18 22:44:51 +0530

    fix: dragging no longer triggers click at the end

---
+++ src/components/DraggableZoomable.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { useGesture } from "@use-gesture/react"
+import { useRef, useState } from "react"
+
+function numInBounds(min: number, max: number, num: number) {
+  return Math.min(Math.max(min, num), max)
+}
+
+export function DraggableZoomable({ children }: { children: React.ReactNode }) {
+  const [offset, setOffset] = useState({ x: 0, y: 0 })
+  const draggingData = useRef({
+    dragging: false,
+    startClientX: 0,
+    startClientY: 0,
+    startOffsetX: 0,
+    startOffsetY: 0,
+  })
+
+  const dragging = useRef(false)
+
+  const [transformOrigin, setTransformOrigin] = useState([0, 0])
+  const [scale, setScale] = useState(1)
+
+  const binds = useGesture(
+    {
+      onDrag: ({ first, last, movement: [dx, dy] }) => {
+        if (first) {
+          draggingData.current = {
+            dragging: true,
+            startClientX: 0,
+            startClientY: 0,
+            startOffsetX: offset.x,
+            startOffsetY: offset.y,
+          }
+          dragging.current = true
+          console.log("drag start")
+        } else if (last) {
+          draggingData.current.dragging = false
+          setTimeout(() => {
+            dragging.current = false
+          }, 0)
+          console.log("drag end")
+        } else {
+          setOffset({
+            x: draggingData.current.startOffsetX + dx,
+            y: draggingData.current.startOffsetY + dy,
+          })
+        }
+      },
+      onClickCapture: ({ event }) => {
+        if (dragging.current) {
+          event.stopPropagation()
+        }
+      },
+      onPinch: ({ delta }) => {
+        setScale((prevScale) => prevScale + delta[0])
+      },
+      onWheel: (e) => {
+        console.log(e)
+        setTransformOrigin([e.event.clientX, e.event.clientY])
+        setScale((prevScale) =>
+          numInBounds(0.1, 2, prevScale - e.delta[1] / 1000),
+        )
+      },
+    },
+    {
+      drag: {
+        filterTaps: true,
+      },
+    },
+  )
+  return (
+    <div
+      className=""
+      {...binds()}
+      style={{
+        transform: `translate(${offset.x}px, ${offset.y}px)`,
+        scale: scale,
+        transformOrigin: `${transformOrigin[0]}px ${transformOrigin[1]}px`,
+        touchAction: "none",
+      }}
+    >
+      {children}
+    </div>
+  )
+}
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,7 +1,8 @@
 "use client"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { useGameContext } from "~/contexts/gameContext"
 import { cn, sleep } from "~/lib/utils"
+import { DraggableZoomable } from "./DraggableZoomable"
 
 const nRows = 100
 const nCols = 100
@@ -10,20 +11,6 @@ export function Grid() {
   const [grid, setGrid] = useState<number[][]>(
     new Array(nRows).fill(new Array(nCols).fill(0)),
   )
-
-  const [xOffset, setXOffset] = useState(0)
-  const [yOffset, setYOffset] = useState(0)
-  const draggingData = useRef({
-    dragging: false,
-    startClientX: 0,
-    startClientY: 0,
-    startOffsetX: 0,
-    startOffsetY: 0,
-  })
-
-  const [transformOrigin, setTransformOrigin] = useState([0, 0])
-
-  const [scale, setScale] = useState(1)
 
   const { setShowHelp, playbackRef } = useGameContext()
 
@@ -39,67 +26,30 @@ export function Grid() {
   }, [])
 
   return (
-    <div
-      className="fixed flex flex-col gap-1 transition-all duration-200"
-      onMouseDown={(e) => {
-        console.log("drag start")
-        draggingData.current = {
-          dragging: true,
-          startClientX: e.clientX,
-          startClientY: e.clientY,
-          startOffsetX: xOffset,
-          startOffsetY: yOffset,
-        }
-      }}
-      onMouseUp={(e) => {
-        draggingData.current.dragging = false
-      }}
-      onMouseMove={(e) => {
-        if (draggingData.current.dragging) {
-          setXOffset(
-            draggingData.current.startOffsetX +
-              e.clientX -
-              draggingData.current.startClientX,
-          )
-          setYOffset(
-            draggingData.current.startOffsetY +
-              e.clientY -
-              draggingData.current.startClientY,
-          )
-          console.log(xOffset, yOffset)
-        }
-      }}
-      style={{
-        top: yOffset,
-        left: xOffset,
-        scale: scale,
-        transformOrigin: `${transformOrigin[0]}px ${transformOrigin[1]}px`,
-      }}
-      onWheel={(e) => {
-        console.log(e.deltaY)
-        setScale((prevScale) => prevScale - e.deltaY / 1000)
-        setTransformOrigin([e.clientX, e.clientY])
-      }}
-    >
-      {grid.map((row, r) => (
-        <div className="flex gap-1" key={r}>
-          {row.map((cell, c) => (
-            <div
-              key={c}
-              className={cn("h-6 w-6 border bg-gray-50", {
-                "bg-gray-400": cell === 1,
-              })}
-              onClick={() => {
-                setGrid((prevGrid) => {
-                  const newGrid = copyArray(prevGrid)
-                  newGrid[r]![c] = 1 - newGrid[r]![c]!
-                  return newGrid
-                })
-              }}
-            />
+    <div className="fixed">
+      <DraggableZoomable>
+        <div className="flex flex-col gap-1">
+          {grid.map((row, r) => (
+            <div className="flex gap-1" key={r}>
+              {row.map((cell, c) => (
+                <div
+                  key={c}
+                  className={cn("h-6 w-6 border bg-gray-50", {
+                    "bg-gray-400": cell === 1,
+                  })}
+                  onClick={() => {
+                    setGrid((prevGrid) => {
+                      const newGrid = copyArray(prevGrid)
+                      newGrid[r]![c] = 1 - newGrid[r]![c]!
+                      return newGrid
+                    })
+                  }}
+                />
+              ))}
+            </div>
           ))}
         </div>
-      ))}
+      </DraggableZoomable>
     </div>
   )
 }
